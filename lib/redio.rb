3c3208043754dc35dc6d70c21a30bc7c8789f0e8
Eric Chapweske <eac@zendesk.com>
2011-08-06 00:40:08 -0600

    On second thought, regular files aren't wanted.

--- lib/redio.rb
+++ lib/redio.rb
@@ -1,7 +1,6 @@
 module Redio
   autoload :Cluster,        'redio/cluster'
   autoload :Expiration,     'redio/expiration'
-  autoload :File,           'redio/file'
   autoload :IO,             'redio/io'
   autoload :TempFile,       'redio/temp_file'
 end
--- lib/redio/temp_file.rb
+++ lib/redio/temp_file.rb
@@ -1,9 +1,21 @@
 module Redio
   class Tempfile < File
+    include IO
     include Expiration
 
     # 3 days
     self.default_expiration = 3 * 86400
+
+    def self.open(key, options = {})
+      new(key, options).tap do |io|
+        yield io
+      end
+    end
+
+    def initialize(key, options)
+      self.key      = key
+      self.redis    = options.fetch(:redis)
+    end
 
   end
 end
